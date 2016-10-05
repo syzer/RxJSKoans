782cc545df4b01d90ab2e317065240b08d80934d
syzer3@gmail.com <syzer3@gmail.com>
2016-10-05 09:51:54 +0200

    03 done

--- test/03_about_querying.js
+++ test/03_about_querying.js
@@ -1,46 +1,46 @@
-var Rx = require('rx'),
+let Rx = require('rx'),
     Observable = Rx.Observable,
-    EventEmitter = require('events').EventEmitter;
+    EventEmitter = require('events').EventEmitter
 
-QUnit.module('Querying');
+// QUnit.module('Querying')
 
-var __ = 'Fill in the blank';
+let __ = 'Fill in the blank'
 
-test('Basic querying', function () {
-  var strings = [];
-  var numbers = Observable.range(1, 100);
+test('Basic querying', () => {
+    let strings = []
+    let numbers = Observable.range(1, 100)
 
-  numbers
-    .filter(function (x) { return x % __ === 0 })
-    .map(function (x) { return x.toString() })
-    .toArray()
-    .subscribe(strings.push.bind(strings));
+    numbers
+        .filter(x => x % 11 === 0)
+        .map(x => x.toString())
+        .toArray()
+        .subscribe(strings.push.bind(strings))
 
-  equal('11,22,33,44,55,66,77,88,99', strings.toString());
-});
+    equal('11,22,33,44,55,66,77,88,99', strings.toString())
+})
 
-test('querying over events', function () {
-  var results = 0;
+test('Querying over events', () => {
+    let results = 0
 
-  var e = new EventEmitter();
-  Observable.fromEvent(e, 'click')
-    .filter(function (click) { return click.x === click.y })
-    .map(function (click) { return __ + __; })
-    .subscribe(function (x) { results = x; });
+    let e = new EventEmitter()
+    Observable.fromEvent(e, 'click')
+        .filter(click => click.x === click.y)
+        .map(click => click.x + click.y)
+        .subscribe(x => results = x)
 
-  e.emit('click', {x: 100, y: 50});
-  e.emit('click', {x: 75,  y: 75});
-  e.emit('click', {x: 40,  y: 80});
+    e.emit('click', {x: 100, y: 50})
+    e.emit('click', {x: 75, y: 75})
+    e.emit('click', {x: 40, y: 80})
 
-  equal(results, 150);
-});
+    equal(results, 150)
+})
 
-test('buffering with count and skip', function () {
-  var results = [];
-  Observable.range(1, 10)
-    .bufferWithCount(__, __)
-    .subscribe(results.push.bind(results));
+test('Buffering with count and skip', () => {
+    let results = []
+    Observable.range(1, 10)
+        .bufferWithCount(5)
+        .subscribe(results.push.bind(results))
 
-  equal('12345',  results[0].join(''));
-  equal('678910', results[1].join(''));
-});
+    equal('12345', results[0].join(''))
+    equal('678910', results[1].join(''))
+})
